48ab70349172be694d2c46982bb695d7b04050b0
amarachiekennia <amarachiekennia@gmail.com>
2022-11-26 05:37:08 -0800

    my js assignment

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let name = "amara";
+let age = 12;
+let height = "12";
+let weight = '138';
+
+console.log(name);
+console.log(12);
+console.log(12);
+console.log(typeof(age));
+console.log(typeof(height));
+console.log(typeof(weight));
+
+let a = 5;
+let b = 15;
+let c = a + b;
+console.log(c);
+
+let i = 5;
+let j = 15;
+let k = a + b;
+alert(k);
+
+
+// Area of a triangle:
+let ba = 10;
+let h = 5;
+let A = ((ba*h)/2);
+console.log(A);
+
+
+// Area of a circle:
+let pi = 3.142;
+let r = 7;
+let t = (pi * (r**2));
+console.log(t);
+
+
+// Conversion of Celsius to Fahrenheit:
+let d = 20;
+let f = (d * (9/5)) + 32;
+console.log(f);
+
+
+// Value of naira in US dollar:
+let usd = 10;
+let na = 500;
+let tot = usd * na;
+console.log(tot)
+
+
+let dolar2naira = 700;
+let us = prompt ("how many dollars do you have?");
+let final = dolar2naira * us;
+alert(`the conversion of your ${us} USD is ${final} Naira.`);
